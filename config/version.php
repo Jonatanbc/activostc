d4a2453454ad6bb4c98281b63d1181051d424366
Jonatan Ba24 <jonatanbc24@gmail.com>
2022-10-04 14:20:17 -0500

    Proyecto inicial

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v5.3.8',
+  'full_app_version' => 'v5.3.8 - build 6619-g1ef336a08',
+  'build_version' => '6619',
+  'prerelease_version' => '',
+  'hash_version' => 'g1ef336a08',
+  'full_hash' => 'v5.3.8-31-g1ef336a08',
+  'branch' => 'master',
+);
